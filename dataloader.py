5d811dfa1b00c2b481d6bbf91c3f8b47771553a0
Patrick Gebert <patrick.gebert@web.de>
2019-02-25 15:04:51 +0100

    Added doctype to dataloader.

--- dataloader.py
+++ dataloader.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
 
-class Dataloader(object): 
+class Dataloader(): 
+    '''Bike Sharing Dataset dataloader.    
+    '''
 
     def __init__(self, csv_path):
+        ''' Initialize Bike Sharing Dataset dataloader.
+        
+        Arguments:
+            csv_path {str} -- Path to the Bike Sharing Dataset CSV file.
+        '''
 
         self.csv_path = csv_path
         self.data = pd.read_csv(self.csv_path)
@@ -12,9 +19,21 @@ class Dataloader(object):
         self.data.sample(frac=1.0, replace=True, random_state=1)
 
     def getHeader(self):
+        ''' Get the column names of the Bike Sharing CSV file.
+        
+        Returns:
+            [list of str] -- the column names of the csv file
+        '''
+
         return list(self.data.columns.values)
 
     def getData(self):
+        ''' Get the pandas frames for the training, validation and test split
+        
+        Returns:
+            [pandas frames] -- the pandas frames for the different splits
+        '''
+
         # Split data into train, validation and test set with 80:10:10 ratio
         split_train = int(80 / 100 * len(self.data)) 
         split_val = int(90 / 100 * len(self.data)) 
@@ -24,4 +43,10 @@ class Dataloader(object):
         return train, val,  test
 
     def getFullData(self):
+        ''' Get all the data in one single pandas frame.
+        
+        Returns:
+            [pandas frame] -- the complete Bike Sharing Dataset data
+        '''
+
         return self.data
